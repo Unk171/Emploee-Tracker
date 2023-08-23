72f14757d4b7f5279c3181c6ae9572438fbfe9ad
Dmitry Dmitry <7353687@gmail.com>
2023-08-22 20:17:46 -0400

    added code with all answers

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2/promise');
+let employees = [];
+let roles = [];
+let departments = [];
+let empId;
+let roleId;
+let depId;
+let addEmpQuery;
+let addRoleQuery;
+let addDepQuery;
 
 const quest1 = [{
   type: 'list',
@@ -15,29 +24,7 @@ const addDepartment = [{
   message: 'What is the name of the department?'
 }];
 
-const addEmployee = [{
-  type: 'input',
-  name: 'addEmployeeFirst',
-  message: "What is the employee's first name?"
-},
-{
-  type: 'input',
-  name: 'addEmployeeLast',
-  message: "What is the employee's last name?"
-},
-{
-  type: 'list',
-  name: 'addEmployeeRole',
-  message: "What is the employee's role?",
-  choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer']
-},
-{
-  type: 'list',
-  name: 'addEmployeeManager',
-  message: "Who is employee's manager?",
-  choices: ['None', 'John Doe', 'Mike Chan', 'Ashley Rodriguez', 'Kevin Tupik', 'Kunak Singh', 'Malia Brown', 'Sarah Lourd', 'Tom Allen']
-}
-];
+
 
 const addRole = [{
   type: 'input',
@@ -68,8 +55,7 @@ const employeeUpdata = [{
   name: 'employeeUpdateRole',
   message: "Which role do you want to assign the selected employee?",
   choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer']
-}
-]
+}]
 
 
 async function main() {
@@ -80,24 +66,18 @@ async function main() {
       password: 'rootr00t!',
       database: 'emploees_db'
     });
-
     console.log('Connected to the employees_db');
-
     const [sqlAll] = await db.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary 
     FROM department JOIN role ON department.id = role.department_id 
     JOIN employee ON role.id = employee.role_id
     ORDER BY employee.id`);
     // console.table(sqlAll);
-
     const [sqlDep] = await db.query(`SELECT * FROM department ORDER BY id`);
     // console.table(sqlDep);
-
     const [sqlRole] = await db.query(`SELECT * FROM role ORDER BY id`);
     // console.table(sqlRole);
-
     const [sqlEmpl] = await db.query(`SELECT * FROM employee ORDER BY id`);
-    // console.table(sqlEmpl);
-
+    // console.table(sqlEmpl); 
     x(sqlAll, sqlDep, sqlRole, sqlEmpl);
   } catch (error) {
     console.error('An error occurred:', error);
@@ -105,59 +85,139 @@ async function main() {
 };
 
 function x(sqlAll, sqlDep, sqlRole, sqlEmpl) {
-
-
   inquirer
     .prompt(quest1)
     .then((answers) => {
-
       if (answers.startQuestion === 'Viev All Employees') {
         console.table(sqlAll)
       } else if (answers.startQuestion === 'View All Roles') {
-        console.table(sqlRole)
+        console.table(sqlRole);
       } else if (answers.startQuestion === 'View All Departments') {
         console.table(sqlDep)
       } else if (answers.startQuestion === 'Add Employee') {
-        console.log(sqlRole)
+        roles = [];
 
-
-
+        for (let i = 0; i < sqlRole.length; i++) {
+          roles.push(sqlRole[i].title)
+        };
         inquirer
           .prompt([{
             type: 'input',
             name: 'addEmployeeFirst',
             message: "What is the employee's first name?"
-          },
-          {
+          }, {
             type: 'input',
             name: 'addEmployeeLast',
             message: "What is the employee's last name?"
-          },
-          {
+          }, {
             type: 'list',
             name: 'addEmployeeRole',
             message: "What is the employee's role?",
-            choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer']
-          },
-          {
+            choices: roles
+          }, {
             type: 'list',
             name: 'addEmployeeManager',
             message: "Who is employee's manager?",
             choices: ['None', 'John Doe', 'Mike Chan', 'Ashley Rodriguez', 'Kevin Tupik', 'Kunak Singh', 'Malia Brown', 'Sarah Lourd', 'Tom Allen']
-          }
-          ])
+          }])
           .then((answers) => {
+            for (let i = 0; i < sqlEmpl.length; i++) {
+              if (answers.addEmployeeManager === sqlEmpl[i].first_name + ' ' + sqlEmpl[i].last_name) {
+                empId = sqlEmpl[i].id;
+              };
+              if (answers.addEmployeeManager === 'None') {
+                empId = null
+              }
+            };
+            for (let i = 0; i < sqlRole.length; i++) {
+              if (answers.addEmployeeRole === sqlRole[i].title) {
+                roleId = sqlRole[i].id;
+              }
+            };
+            addEmpQuery = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ${answers.addEmployeeFirst}, ${answers.addEmployeeLast}, ${roleId}, ${empId}`
+          })
+      } else if (answers.startQuestion === 'Add Role') {
+        departments = [];
+        for (let i = 0; i < sqlDep.length; i++) {
+          departments.push(sqlDep[i].name)
+        };
+        inquirer
+          .prompt([{
+            type: 'input',
+            name: 'roleName',
+            message: 'What is the name of the role?'
+          }, {
+            type: 'input',
+            name: 'addSalary',
+            message: 'What is the salary of the role?'
+          }, {
+            type: 'list',
+            name: 'addRoleDepartment',
+            message: 'Which department does the role belong to?',
+            choices: departments
+          }])
+          .then((answers) => {
+            for (let i = 0; i < sqlDep.length; i++) {
+              if (answers.addRoleDepartment === sqlDep[i].name) {
+                depId = sqlDep[i].id;
+              }
+            };
+            addRoleQuery = `INSERT INTO role (title, salary, department_id) VALUES ${answers.roleName}, ${answers.addSalary}, ${depId}}`
+          })
+      } else if (answers.startQuestion === 'Add Department') {
+        inquirer
+          .prompt([{
+            type: 'input',
+            name: 'addDepName',
+            message: 'What is the name of the department?'
+          }])
+          .then((answers) => {
+            addDepQuery = `INSERT INTO department (name) VALUES ${answers.addDepName}`
+          })
+      } else if (answers.startQuestion === 'Update Employee Role') {
+        roles = [];
+        employees = [];
+        empId = '';
+        roleId = '';
 
-            console.log("answers:", answers)
-            const params =
-              db.query(`INSERT INTO emloyee (id, first_name, last_name, role_id, manager_id) VALUES (?)`,)
+        for (let i = 0; i < sqlEmpl.length; i++) {
+          employees.push(sqlEmpl[i].first_name + ' ' + sqlEmpl[i].last_name)
+        };
 
-          }
+        for (let i = 0; i < sqlRole.length; i++) {
+          roles.push(sqlRole[i].title)
+        };
 
-          )
+        inquirer
+          .prompt([{
+            type: 'list',
+            name: 'employeeUpdate',
+            message: "Which employee's role do you want update?",
+            choices: employees
+          },
+          {
+            type: 'list',
+            name: 'employeeUpdateRole',
+            message: "Which role do you want to assign the selected employee?",
+            choices: roles
+          }])
+          .then((answers) => {
+            for (let i = 0; i < sqlEmpl.length; i++) {
+              if (answers.employeeUpdate === sqlEmpl[i].first_name + ' ' + sqlEmpl[i].last_name) {
+                empId = sqlEmpl[i].id;
+              }
+            };
+            for (let i = 0; i < sqlRole.length; i++) {
+              if (answers.employeeUpdateRole === sqlRole[i].title) {
+                roleId = sqlRole[i].id;
+              }
+            };
+            addDepQuery = `UPDATE employee SET role_id = ${roleId} WHERE id = ${empId};`
+            console.log("addDepQuery:", addDepQuery)
+          })
 
 
-      };
+      }
     })
 }
 
